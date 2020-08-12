10bebdef47bd707bfd9d8edd2aae030f8f1fc297
cur-114 <cursan0409@gmail.com>
2020-08-12 18:13:14 +0900

    motd

--- src/main/java/net/rezxis/mchosting/lobby/ServerListener.java
+++ src/main/java/net/rezxis/mchosting/lobby/ServerListener.java
@@ -70,7 +70,8 @@ public class ServerListener implements Listener {
 	
 	@EventHandler
 	public void onPing(ServerListPingEvent e) {
-		e.setMotd(ChatColor.BOLD+"1234567890qwertyuiopasdfghjklzxcvbnm1234567890qwertyuiopasdfghjklzxcvbnm");
+		e.setMotd(ChatColor.AQUA+""+ChatColor.BOLD+"        ---==REZXIS NETWORK==---\n"
+				+ChatColor.LIGHT_PURPLE+""+ChatColor.BOLD+""+ChatColor.UNDERLINE+"            ---==REALM==---");
 		/*e.setMotd(ChatColor.AQUA+""+ChatColor.BOLD+"                     ----====REZXIS NETWORK====----"+"\n"+
 		ChatColor.LIGHT_PURPLE+""+ChatColor.BOLD+""+ChatColor.UNDERLINE+"                             --==REALM==--");*/
 	}
